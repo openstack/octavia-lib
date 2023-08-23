2a84a218ef1f683d5584784b7d9607e7453b0729
Michael Johnson <johnsomor@gmail.com>
2023-08-23 00:13:39 +0000

    Fix a possible receive timeout
    This patch fixes a possible receive timeout caused by a slow response from the
    driver agent. For example if the database is very slow.
    
    Closes-Bug: #2032890
    
    Change-Id: I9079030a5fef9dc44da242adab3c568666777451

--- octavia_lib/tests/unit/api/drivers/test_driver_lib.py
+++ octavia_lib/tests/unit/api/drivers/test_driver_lib.py
@@ -11,6 +11,7 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
+import socket
 from unittest import mock
 
 from octavia_lib.api.drivers import driver_lib
@@ -45,7 +46,7 @@ class TestDriverLib(base.TestCase):
     @mock.patch('builtins.memoryview')
     def test_recv(self, mock_memoryview):
         mock_socket = mock.MagicMock()
-        mock_socket.recv.side_effect = [b'1', b'\n', b'2', b'\n', b'3', b'\n']
+        mock_socket.recv.side_effect = [b'1', b'\n', b'2', b'3', b'\n']
         mock_socket.recv_into.return_value = 1
         mv_mock = mock.MagicMock()
         mock_memoryview.return_value = mv_mock
@@ -70,6 +71,25 @@ class TestDriverLib(base.TestCase):
             # Test payload recv timeout
             self.assertRaises(driver_exceptions.DriverAgentTimeout,
                               self.driver_lib._recv, mock_socket)
+
+    @mock.patch('builtins.memoryview')
+    def test_recv_with_timeout(self, mock_memoryview):
+        mock_socket = mock.MagicMock()
+        mock_socket.recv.side_effect = [socket.timeout, b'1', b'\n', b'2',
+                                        b'3', b'\n']
+        mock_socket.recv_into.return_value = 1
+        mv_mock = mock.MagicMock()
+        mock_memoryview.return_value = mv_mock
+        mv_mock.tobytes.return_value = b'"test data"'
+
+        response = self.driver_lib._recv(mock_socket)
+
+        calls = [mock.call(1), mock.call(1)]
+
+        mock_socket.recv.assert_has_calls(calls)
+        mock_socket.recv_into.assert_called_once_with(
+            mv_mock.__getitem__(), 1)
+        self.assertEqual('test data', response)
 
     @mock.patch('octavia_lib.api.drivers.driver_lib.DriverLibrary._recv')
     def test_send(self, mock_recv):
